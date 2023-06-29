6c37b0cd49c9030829e78eee6f546eadebe2abd8
Johnny Chang <johnny22375@gmail.com>
2023-06-30 01:21:50 +0800

    finished nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,14 @@
 import "../styles/NavBar.scss";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 function NavBar() {
   return (
     <div id="NavBar">
       <div className="logo">LOGO</div>
-      <div className="setting"> <FontAwesomeIcon icon="gear" /></div>
+      <div className="setting">
+        <img src={require("../imgs/gear.svg").default} alt="gear icon" />
+      </div>
     </div>
   )
 }
