732f55da9a1ea2498269ce87b17f6a50b3973b41
Johnny Chang <johnny22375@gmail.com>
2023-06-30 02:13:31 +0800

    styles changed

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -3,33 +3,36 @@ import "../styles/DashBoard.scss";
 function DashBoard() {
   return (
     <div id="DashBoard">
-      <img className="taiwan-logo" src={require("../imgs/taiwan.svg").default} alt="taiwan logo" />
+      <div className="taiwan-logo">
+        <img src={require("../imgs/taiwan.svg").default} alt="taiwan logo" />
+      </div>
       <div className="content">
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
-        <h1>This is the contentcontentcontentcontentcontentcontent</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
+        <h1>this is just an exmaple for usagekdj alksdjf dafklhf</h1>
       </div>
     </div>
   );
