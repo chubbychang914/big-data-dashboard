d924455c48fb8cedb90451f6321cfe2299bef8f2
Johnny Chang <johnny22375@gmail.com>
2023-07-01 01:12:44 +0800

    changed all to select

--- src/components/MainForm.jsx
+++ src/components/MainForm.jsx
@@ -15,19 +15,34 @@ const MainForm = () => {
 
   return (
     <form id="MainForm" onSubmit={handleSubmit}>
-      <div className="yearSelect">
-        <div className="">年份</div>
-        <input type="text" value={year} onChange={e => setYear(e.target.value)} />
+      <div className="year-select">
+        <div className="year-label">年份</div>
+        <select value={year} onChange={e => setYear(e.target.value)}>
+          <option value="">請選擇</option>
+          <option value="110">110</option>
+          <option value="111">111</option>
+          <option value="112">112</option>
+        </select>
       </div>
       {/*  */}
-      <div className="citySelect">
-        <div className="">縣市</div>
-        <input type="text" value={city} onChange={e => setCity(e.target.value)} />
+      <div className="city-select">
+        <div className="city-label">縣市</div>
+        <select value={city} onChange={e => setCity(e.target.value)}>
+          <option value="">請選擇</option>
+          <option value="tc">tc</option>
+          <option value="tp">tp</option>
+          <option value="tn">tn</option>
+        </select>
       </div>
       {/*  */}
-      <div className="countySelect">
-        <div className="">區</div>
-        <input type="text" value={county} onChange={e => setCounty(e.target.value)} />
+      <div className="county-select">
+        <div className="county-label">區</div>
+        <select value={county} onChange={e => setCounty(e.target.value)}>
+          <option value="">請選擇</option>
+          <option value="shalu">sl</option>
+          <option value="chingshui">cs</option>
+          <option value="hello">hl</option>
+        </select>
       </div>
       {/* submit btn */}
       <button type="submit">Submit</button>
